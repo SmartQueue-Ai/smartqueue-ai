a2033497dd0826815f207d127f5782afc4c5f0c1
DiptanshuVishwa <diptanshuvishwa364@gmail.com>
2026-07-27 01:26:14 +0530

    feat(inventory): bootstrap inventory service

--- backend/inventory-service/src/test/java/com/smartqueue/inventory_service/InventoryServiceApplicationTests.java
+++ backend/inventory-service/src/test/java/com/smartqueue/inventory_service/InventoryServiceApplicationTests.java
@@ -1,12 +1,29 @@
 package com.smartqueue.inventory_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class InventoryServiceApplicationTests {
+
+    @MockBean
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockBean
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @MockBean
+    private StringRedisTemplate stringRedisTemplate;
+
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
